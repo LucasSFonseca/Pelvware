7b450a9a0c0bb84b1700fcf6d276c2a9769a7316
Lucas Fonseca <luquinhas0805@gmail.com>
2018-06-15 14:41:40 -0300

    adicionando código do hardware

---
+++ ESP Pelvware/sketch_jun01a/sketch_jun01a.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include "FS.h"
+#include <ESP8266FtpServer.h>
+
+const char* ssid = "ESP_Pelv";
+const char* password = "pelvware123";
+
+FtpServer ftpSrv;
+
+void setup() 
+{
+  Serial.begin(115200);
+  
+  /*
+   * Code for the WiFi Connection.
+   * 
+   * 
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  */
+
+  
+  /*
+   * Code used for truning the ESP into a WiFi Hotspot (Access Point).
+   * 
+   */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+
+
+  Serial.println(myIP);
+  
+  // Initializing FTPServer and SPIFFS.
+  if(SPIFFS.begin()){
+    ftpSrv.begin("admin", "admin"); // Username and password.
+  }
+  SPIFFS.format();
+
+  for(int i = 0; i < 100 ; i++){
+    File f = SPIFFS.open("/teste", "a");
+    String name = f.name();
+    if(!f) {
+      Serial.println("File open failed");
+    }
+    else{
+      f.print(i);
+      f.print(";");
+      f.println(i*100);
+    }
+    f.close();
+  }
+  
+}
+
+/*
+ * Writing on a file in the SPIFFS.
+ * 
+ * 
+void loop() 
+{
+  Serial.println("Starting...");
+  File f = SPIFFS.open("/teste.txt", "a");
+  String name = f.name();
+  if(!f) {
+    Serial.println("File open failed");
+  }
+  else{
+    f.println("Oi");
+    Serial.println("File " + name + " open");
+  }
+  f.close();
+  Serial.println("File " + name + " closed");
+  
+}*/
+
+
+
+/*
+ * Reading a file from the SPIFFS
+ * 
+ * 
+void loop(){
+  File f = SPIFFS.open("/teste.txt", "r");
+  if(!f){
+    Serial.println("File open failed");
+  }
+  else {
+    String content = f.readStringUntil('\r');
+    Serial.println(content);
+  }
+  f.close();
+}*/
+
+/*
+ * FtpServer handle, used to manage the requests for files.
+ * 
+ *
+ *
+  */
+void loop(){
+    ftpSrv.handleFTP();
+    
+}
+
